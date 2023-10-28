aaedb0f072d93fc3d69ae20eda3114d5bb0216d8
bilguundalaim <bilguundalai3355@gmail.com>
2023-10-28 22:15:06 +0800

    hha

--- src/components/AddSmth/AddSmth.jsx
+++ src/components/AddSmth/AddSmth.jsx
@@ -1,18 +1,25 @@
-import { IoAdd } from "react-icons/io5"
+import { IoAdd } from "react-icons/io5";
 import PropTypes from "prop-types";
-import styles from './AddSmth.module.css';
+import styles from "./AddSmth.module.css";
+import AddTask from "../AddTask/AddTask";
+import { useState } from "react";
 
 const AddSmth = ({ smth }) => {
+  const [isVisibleAddTask, setIsVisibleAddTask] = useState(false);
+
+  const handleAddSmthClick = () => setIsVisibleAddTask(!isVisibleAddTask);
+
   return (
-    <div className={styles.container}>
-      <IoAdd size={30}/>
+    <div className={styles.container} onClick={handleAddSmthClick}>
+      <IoAdd size={30} />
       <h3>Add New {smth}</h3>
+      {isVisibleAddTask && <AddTask />}
     </div>
-  )
-}
+  );
+};
 
 AddSmth.propTypes = {
   smth: PropTypes.string.isRequired,
-}
+};
 
-export default AddSmth
+export default AddSmth;
--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -1,8 +1,13 @@
+import styles from "./Content.module.css";
 // import Task from "../Task/Task";
-// import AddSmth from "../AddSmth/AddSmth";
+import AddSmth from "../AddSmth/AddSmth";
 
 const Content = () => {
-  return <div>Content</div>;
+  return(
+    <div className={styles.container}>
+      <AddSmth smth={"Task"}/>
+    </div>
+  );
 };
 
 export default Content;
